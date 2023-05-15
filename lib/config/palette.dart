1d8464efdd88212e7c799eddef7eb1a75451044c
2022056562 <2022056562@hanyang.ac.kr>
2023-05-15 20:03:43 +0900

    changes

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -6,6 +6,5 @@ class Palette {
   static const Color textColor2 = Color(0XFF9BB3C0);
   static const Color facebookColor = Color(0xFF3B5999);
   static const Color googleColor = Color(0xFF4285F4);
-  static const Color backgroundColor =
-  Color(0xFFECF3F9);
+  static const Color backgroundColor = Color(0xFFECF3F9);
 }
